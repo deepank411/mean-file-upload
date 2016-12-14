628d5e8405aa71d50d7fa1d533f6aea221636990
Shubham <8shubhamkothari@gmail.com>
2016-12-15 01:31:28 +0530

    added some more api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express(); //defining our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_db'); // connecting to our database
+mongoose.connect('mongodb://127.0.0.1/my_db'); // connecting to our database
 
 var song = require('./models/songs');
 //configuiration of our app to use body parser
@@ -25,10 +25,6 @@ router.get('/', function(req, res){
     res.json({ message: 'Hurray! Its Working.'});
 });
 
-//if someone goes to /songs
-router.get('/songs', function(req, res){
-    res.sendfile('./views/newSongForm.html');
-});
 //--routes starting with /songs--//
 router.route('/songs')
 
@@ -51,21 +47,61 @@ router.route('/songs')
     //route to get the list of all the songs
     .get(function(req, res){
         //var newSong = new song();
-        song.find(function(err, response){
+        song.find(function(err, newSong){
             if(err)
                 res.send(err);
-
-                res.json(response);
+            else
+                res.json(newSong);
         });
     });
 
+//--routes that end with "/songs/id"--//
+router.route('/songs/:song_id')
 
+    .get(function(req, res){
+        song.findById(req.params.song_id, function(err, newSong){
+            if(err)
+                res.send(err);
+            else
+                res.json(newSong);
+        });
+    });
 
+    .put(function(req, res){
+        song.findById(req.params.song_id, function(err, newSong){
+            if(err)
+                res.send(err);
+            else {
+                newSong.name = req.body.name ; //updating the name of the song
+
+                newSong.save(function(err){
+                    if(err)
+                        res.send(err);
+
+                    else {
+                        res.json({message: 'Song Updated!'});
+                    }
+                });
+            }
+        });
+    });
+
+    .delete(function(req, res){
+        song.remove({
+            _id : req.params.song_id;
+        }, function(err, newSong){
+            if(err)
+                res.send(err);
+
+            else {
+                res.json({message: 'Song Successfully Deleted!'});
+            }
+        });
+
+    });
 
 //--REGISTER OUR ROUTES --//
-//all our routes will be prefixed with /song
-app.use('/home', router);
-
+app.use('/', router);
 //------------------------------- Start the server --------------//
 app.listen(port);
 console.log('Server running at :' + port);
